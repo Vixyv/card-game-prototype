b733741f79de78178312a692cbc2a7b7acd17bf0
vixyv <76610866+Vixyv@users.noreply.github.com>
2025-11-20 09:54:18 -0800

    Light section

--- scripts/player_character.gd
+++ scripts/player_character.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 ## Speed in pixels per second.
 @export_range(0, 1000) var speed := 60
 
@@ -10,14 +9,16 @@ func _physics_process(_delta: float) -> void:
 	if move_and_slide():
 		resolve_collisions()
 
-func get_player_input() -> void:
-	var vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = vector * speed
 
 func resolve_collisions() -> void:
 	for i in get_slide_collision_count():
 		var collision := get_slide_collision(i)
-		var body := collision.get_collider() as RigidBody2D
+		var body := collision.get_collider() as MovableObject
 		if body:
-			body.apply_force(-100.0 * collision.get_normal())
+			body.apply_impact(velocity)
+
+
+func get_player_input() -> void:
+	var vector := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = vector * speed
 	
