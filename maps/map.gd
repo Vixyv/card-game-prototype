f5daba111794ad7dc62314d67a2e295370450540
vixyv <76610866+Vixyv@users.noreply.github.com>
2025-12-02 10:10:26 -0800

    Finished extra map info

--- maps/map.gd
+++ maps/map.gd
@@ -7,12 +7,14 @@ extends Node
 @export var detectors: Node2D
 
 const KEY_BEST_TIME := "best_time"
+const KEY_BEST_TRAVEL_DISTANCE := "best_travel_distance"
 
 var registered_detector_count := 0
 var valid_detector_count := 0
 var current_map_time := 0.0
 var map_save_data: Dictionary
 
+var player_character: PlayerCharacter
 
 func _ready() -> void:	
 	for child_node in detectors.get_children():
@@ -21,28 +23,51 @@ func _ready() -> void:
 			registered_detector_count += 1
 			detector.validity_changed.connect(_on_detector_validity_changed)
 	
+	for child_node in get_children():
+		var pc := child_node as PlayerCharacter
+		if pc:
+			player_character = pc
+	
 	Main.map_loaded(self)
 	HUD.show_map_info(self)
+	HUD.show_map_detector_count(
+		valid_detector_count, registered_detector_count
+	)	
 
 func _process(delta: float) -> void:
 	current_map_time += delta
 	HUD.show_map_time(current_map_time)
 
+
 func _on_detector_validity_changed(valid: bool) -> void:
 	if valid:
 		valid_detector_count += 1
 		if valid_detector_count == registered_detector_count:
-			_update_best_time()
+			_update_best_scores()
 			Main.load_next_map()
 	else:
 		valid_detector_count -= 1
+	HUD.show_map_detector_count(
+		valid_detector_count, registered_detector_count
+	)
 
-
-func _update_best_time() -> void:
-	var new_best_time := floori(current_map_time)
+func _update_best_scores() -> void:
+	var new_time := floori(current_map_time)
 	if (
 			not map_save_data.has(KEY_BEST_TIME)
-			or new_best_time <= map_save_data[KEY_BEST_TIME]
+			or new_time < map_save_data[KEY_BEST_TIME]
 	):
-		map_save_data[KEY_BEST_TIME] = new_best_time
-		HUD.show_map_best_time(new_best_time)
+		map_save_data[KEY_BEST_TIME] = new_time
+	else:
+		new_time = -1
+
+	var new_travel_distance := floori(player_character.travel_distance)
+	if (
+			not map_save_data.has(KEY_BEST_TRAVEL_DISTANCE)
+			or new_travel_distance < map_save_data[KEY_BEST_TRAVEL_DISTANCE]
+	):
+		map_save_data[KEY_BEST_TRAVEL_DISTANCE] = new_travel_distance
+	else:
+		new_travel_distance = -1
+
+	HUD.show_map_best_scores(new_time, new_travel_distance)
--- player/player_character.gd
+++ player/player_character.gd
@@ -1,14 +1,27 @@
+class_name PlayerCharacter
 extends CharacterBody2D
 
 ## Speed in pixels per second.
 @export_range(0, 1000) var speed := 60
 
+var last_position: Vector2
+var travel_distance := 0.0
+
+
+func _ready() -> void:
+	last_position = position
+	HUD.show_map_travel_distance(travel_distance)
 
 func _physics_process(_delta: float) -> void:
 	get_player_input()
 	if move_and_slide():
 		resolve_collisions()
+		
+	travel_distance += last_position.distance_to(position)
+	last_position = position
 
+func _process(_delta: float) -> void:
+	HUD.show_map_travel_distance(travel_distance)
 
 func resolve_collisions() -> void:
 	for i in get_slide_collision_count():
