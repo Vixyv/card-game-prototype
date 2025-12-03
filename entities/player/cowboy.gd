0a0fcba4333d12e56242ee346b0fae9ab83335a9
vixyv <76610866+Vixyv@users.noreply.github.com>
2025-12-03 10:55:15 -0800

    Added movement

--- entities/player/cowboy.gd
+++ entities/player/cowboy.gd
@@ -1,25 +1,25 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+## Speed in pixels per second.
+var speed := 60
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _physics_process(_delta: float) -> void:
+	get_player_input()
 	move_and_slide()
+
+
+"""
+func resolve_collisions() -> void:
+	for i in get_slide_collision_count():
+		var collision := get_slide_collision(i)
+		var body := collision.get_collider() as MovableObject
+		if body:
+			body.apply_impact(velocity)
+"""
+
+
+func get_player_input() -> void:
+	var vector := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = vector * speed
