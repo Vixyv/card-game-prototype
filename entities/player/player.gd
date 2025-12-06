e50f078f35932369b6038c9f4aff0ae273fd0919
vixyv <76610866+Vixyv@users.noreply.github.com>
2025-12-05 17:41:21 -0800

    Added (bad) camera interp
    Between player and mouse positions

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -5,24 +5,20 @@ extends CharacterBody2D
 
 ## Speed in pixels per second.
 @export var speed := 60
-## DELETE LATER
-@export var facing_dir_sprite: Sprite2D
-
 @export var sprite := Sprite2D
+
+@export var facing_dir_sprite: Sprite2D ## DELETE LATER
+
 
 const PI_DIV_2 = PI/2
 ## Degree range for which the sprite faces up or down (plus and minus)
-@export var up_down_degree_range := 25.0
+@export_range(0, 89) var up_down_degree_range := 23.0
 var up_down_rad_range: float
 
 ## Degree range for which the sprite back left or right (plus and minus).
 ## Up down range takes priority
-@export var back_lr_degree_range := 50.0
+@export_range(0, 89) var back_lr_degree_range := 46.0
 var back_lr_rad_range: float
-
-# 1 = facing right, -1 = facing left
-# Can't use scale.x bc it resets to 1.0 for some reason??
-var facing_dir := 1
 
 
 func _ready() -> void:
@@ -32,6 +28,8 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	_rotate_sprite()
 
+# Rotates the sprite to one of 4 direction:
+#   Forward, back, forward right, and back right
 func _rotate_sprite() -> void:
 	# The facing dir of the player (l/r) changes the sign of `get_angle_to`
 	var rot_to_mouse = sign(scale.y) * get_angle_to(get_global_mouse_position())
@@ -43,26 +41,26 @@ func _rotate_sprite() -> void:
 		rot_to_mouse > sign_of_rtm * PI_DIV_2 - up_down_rad_range
 	):
 		if rot_to_mouse < 0:
-			facing_dir_sprite.rotation = -PI_DIV_2 # Looking forward (down)
+			facing_dir_sprite.rotation = -PI_DIV_2 # Looking forward
 		else:
-			facing_dir_sprite.rotation = PI_DIV_2  # Looking back (up)
+			facing_dir_sprite.rotation = PI_DIV_2  # Looking back
 	elif ( # When mouse is above and further left or right
 		rot_to_mouse < 0 and 
 		rot_to_mouse < -PI_DIV_2 + back_lr_rad_range and 
 		rot_to_mouse > -PI_DIV_2 - back_lr_rad_range
 	):
 		facing_dir_sprite.rotation = -PI/4 # Looking back right
-		_flip_sprite(rot_to_mouse)
+		_flip_player(rot_to_mouse)
 	else:
 		facing_dir_sprite.rotation = PI/4  # Looking forward right
-		_flip_sprite(rot_to_mouse)
+		_flip_player(rot_to_mouse)
 
-func _flip_sprite(rot_to_mouse: float) -> void:
+# Flips the whole player
+func _flip_player(rot_to_mouse: float) -> void:
 	if abs(rot_to_mouse) > PI_DIV_2 + up_down_rad_range:
 		scale.x = -1
 
 
-# TODO: Implement functionality for moving and rotating the revolver
 func _physics_process(_delta: float) -> void:
 	_handle_player_input()
 	move_and_slide()
--- entities/player/player_camera.gd
+++ entities/player/player_camera.gd
@@ -2,13 +2,17 @@ extends Camera2D
 
 
 @export var player: Player
+## Percentage to interpolate from the player position to the mouse position.
+@export_range(0, 1) var mouse_interp_amount := 0.2
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-# TODO: Add camera interp between player and mouse
+# TODO: Fix camera interp between player and mouse
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	position = player.position
+	position = lerp(player.position, get_global_mouse_position(), mouse_interp_amount)
+	#position.x = smoothstep(player.position.x, get_global_mouse_position().x, mouse_interp_amount)
+	#position.y = smoothstep(player.position.y, get_global_mouse_position().y, mouse_interp_amount)
